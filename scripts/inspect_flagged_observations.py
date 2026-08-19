f732fe8c084f0cc1c1fb37a56edad12f52baeec0
DanavathGanesh <danavathganesh222@gmail.com>
2026-08-19 18:30:35 +0530

    Update inspect_flagged_observations.py

--- scripts/inspect_flagged_observations.py
+++ scripts/inspect_flagged_observations.py
@@ -1,2 +1,7 @@
 import pandas as pd
 from pathlib import Path
+# Project directories
+PROJECT_ROOT = Path("/content/Round-2-Cotton-PyFAO56")
+
+RAW_DIR = PROJECT_ROOT / "data" / "raw"
+PROCESSED_DIR = PROJECT_ROOT / "data" / "processed"
